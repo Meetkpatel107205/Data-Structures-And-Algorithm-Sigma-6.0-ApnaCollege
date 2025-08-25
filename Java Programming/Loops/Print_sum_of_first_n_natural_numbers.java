578df700233942df6bbfc6bb172bacf6b0726d3e
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-25 22:15:38 +0530

    Upload All Files Till 25/08/2025

---
+++ Java Programming/Loops/Print_sum_of_first_n_natural_numbers.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Print_sum_of_first_n_natural_numbers
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Any Number : ");
+        int n = sc.nextInt();
+
+        int counter = 1;
+
+        int sumOfNNatural = 0;
+
+        while(counter <= n)
+        {
+            sumOfNNatural += counter;
+            counter++;
+        }
+
+        System.out.println("\nSum of first " + n + " natural numbers is : " + sumOfNNatural);
+    }    
+}
