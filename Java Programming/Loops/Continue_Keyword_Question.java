e104d0e127f08b46e28b59f2ec1af8e3542f21b3
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-27 13:26:34 +0530

    Stop Tracking .cph Folder

---
+++ Java Programming/Loops/Continue_Keyword_Question.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Continue_Keyword_Question
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        do
+        {
+            System.out.print("\nEnter Any Number : ");
+            int temp = sc.nextInt();
+
+            if(temp % 10 == 0)
+            {
+                continue;
+            }
+
+            System.out.println();
+
+            System.out.println(temp);
+        }while(true);
+    }    
+}
